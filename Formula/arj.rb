8e1e96c4635ac97eac074cc17eadfcfe6037810d
Serg Merenkov <mer.sergei@gmail.com>
2026-05-10 22:50:15 +0300

    arj 1.1.2

--- Formula/arj.rb
+++ Formula/arj.rb
@@ -1,8 +1,8 @@
 class Arj < Formula
   desc "Archive utility compatible with ARJ format for macOS"
   homepage "https://github.com/merenkoff/ARJ.swift"
-  url "https://github.com/merenkoff/ARJ.swift/archive/refs/tags/1.1.0.tar.gz"
-  sha256 "d4c6a766c0d9657cf58035fdcde8d1531bbc10df56069adcb03b711b1b801a31"
+  url "https://github.com/merenkoff/ARJ.swift/archive/refs/tags/1.1.2.tar.gz"
+  sha256 "16e5410e027a01d71e6e1e66d730a0edf2e0fc9235ab7a26a811b8899cf483b9"
   license "OwnNet Source License 1.0"
 
   depends_on :macos
